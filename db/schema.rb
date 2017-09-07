9d67f606d51b5a63ea121638a8b77b7e3e05d75b
Chris Mason <chris@chaione.com>
2017-09-07 11:24:20 -0500

    Add device uuid to markers
    This allows us to identify markers created by the same device so
    users can edit their markers without requiring any authentication.
    
    Conflicts:
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,9 @@ ActiveRecord::Schema.define(version: 20170909082743) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.jsonb "data", default: {}, null: false
+    t.string "device_uuid", default: "", null: false
     t.index ["category"], name: "index_connect_markers_on_category"
+    t.index ["device_uuid"], name: "index_connect_markers_on_device_uuid"
     t.index ["latitude", "longitude"], name: "index_connect_markers_on_latitude_and_longitude"
     t.index ["resolved"], name: "index_connect_markers_on_unresolved", where: "(resolved IS FALSE)"
   end
